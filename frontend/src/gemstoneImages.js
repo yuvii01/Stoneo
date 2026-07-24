67ad863ea7789d95a7598d12740a92fb4be367e9
yuvii01 <yuvrajmaheshwari07@gmail.com>
2026-07-24 20:53:08 +0530

    fix

--- frontend/src/gemstoneImages.js
+++ frontend/src/gemstoneImages.js
@@ -58,5 +58,23 @@ export const gemstoneImages = {
     "/gemstones/jasper/desertjasper.jpg",
     "/gemstones/jasper/shellfintjasper.jpg",
     "/gemstones/jasper/angeljasper.jpg"
+  ],
+  "table-top": [
+    "https://i.pinimg.com/736x/68/84/b8/6884b808b96bd2ce7cd0ae904d502dbe.jpg",
+    "https://images.unsplash.com/photo-1577140917170-285929fb55b7?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+    "https://images.unsplash.com/photo-1604578762246-41134e37f9cc?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+    "https://images.unsplash.com/photo-1599696848652-f0ff23bc911f?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+  ],
+  "wash-basin": [
+    "https://i.pinimg.com/1200x/78/68/8b/78688bb5a28888d87cfa8c03d0c065ca.jpg",
+    "https://images.unsplash.com/photo-1584622650111-993a426fbf0a?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+    "https://images.unsplash.com/photo-1620626011761-996317b8d101?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+    "https://images.unsplash.com/photo-1600566752355-35792bedcfea?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+  ],
+  "back-panel": [
+    "https://i.pinimg.com/736x/79/d7/f9/79d7f9b997fe48ff5cc92532e2bc8811.jpg",
+    "https://images.unsplash.com/photo-1600585154526-990dced4ea0d?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+    "https://images.unsplash.com/photo-1600607686527-6fb886090705?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+    "https://images.unsplash.com/photo-1600566753086-00f18efc2291?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
   ]
 };
