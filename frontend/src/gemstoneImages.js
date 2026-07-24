875aba19300e106c4c5b77747e1b0c70cf4a3b88
yuvii01 <yuvrajmaheshwari07@gmail.com>
2026-07-24 10:04:12 +0530

    fix

---
+++ frontend/src/gemstoneImages.js
@@ -0,0 +1,62 @@
+export const gemstoneImages = {
+  "agate": [
+    "/gemstones/agate/indianblueagate.jpg",
+    "/gemstones/agate/blackagate.jpg",
+    "/gemstones/agate/mixagate.jpg",
+    "/gemstones/agate/crystalagate.jpg",
+    "/gemstones/agate/greyagate.jpg",
+    "/gemstones/agate/yellowagate.jpg",
+    "/gemstones/agate/brownagate.jpg",
+    "/gemstones/agate/mossagate.jpg",
+    "/gemstones/agate/fossilagate.jpg"
+  ],
+  "quartz": [
+    "/gemstones/quartz/whitequartz.jpg",
+    "/gemstones/quartz/smokeyquartz.jpg",
+    "/gemstones/quartz/rosequartz.jpg",
+    "/gemstones/quartz/greenaventurine.jpg",
+    "/gemstones/quartz/bluecalcite.jpg",
+    "/gemstones/quartz/greenfluorite.jpg",
+    "/gemstones/quartz/hemotiedquartz.jpg",
+    "/gemstones/quartz/amethyst.jpg",
+    "/gemstones/quartz/goldenquartz.jpg",
+    "/gemstones/quartz/lightgoldenquartz.jpg",
+    "/gemstones/quartz/crystalquartz.jpg",
+    "/gemstones/quartz/smokeyquartzdark.jpg"
+  ],
+  "gemstone": [
+    "/gemstones/gemstone/sodaliteblue.jpg",
+    "/gemstones/gemstone/tigereye.jpg",
+    "/gemstones/gemstone/lapizlazuli.jpg",
+    "/gemstones/gemstone/septaria.jpg",
+    "/gemstones/gemstone/sodalitepurple.jpg",
+    "/gemstones/gemstone/tigereyeblue.jpg",
+    "/gemstones/gemstone/amazonite.jpg",
+    "/gemstones/gemstone/blueapatite.jpg",
+    "/gemstones/gemstone/blackobsidian.jpg",
+    "/gemstones/gemstone/blackobsidiansilver.jpg",
+    "/gemstones/gemstone/labrodorite.jpg",
+    "/gemstones/gemstone/malachiteflower.jpg"
+  ],
+  "shellstone": [
+    "/gemstones/shellstone/goldenmop.jpg",
+    "/gemstones/shellstone/blackmop.jpg",
+    "/gemstones/shellstone/whitemop.jpg",
+    "/gemstones/shellstone/greenabelone.jpg"
+  ],
+  "fossil": [
+    "/gemstones/fossil/petrifiedwood.jpg",
+    "/gemstones/fossil/yellowfossil.jpg",
+    "/gemstones/fossil/whitepetrifiedwood.jpg",
+    "/gemstones/fossil/petrifiedwoodretro.jpg"
+  ],
+  "jasper": [
+    "/gemstones/jasper/redjasper.jpg",
+    "/gemstones/jasper/jamacianjasper.jpg",
+    "/gemstones/jasper/mockitejasper.jpg",
+    "/gemstones/jasper/greenjasper.jpg",
+    "/gemstones/jasper/desertjasper.jpg",
+    "/gemstones/jasper/shellfintjasper.jpg",
+    "/gemstones/jasper/angeljasper.jpg"
+  ]
+};
